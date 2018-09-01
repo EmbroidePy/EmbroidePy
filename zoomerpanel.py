2539543cec73d97d720e863e70bb6c7828aac3d9
tatarize <tatarize@gmail.com>
2018-09-01 00:12:33 -0700

    Add files via upload

--- zoomerpanel.py
+++ zoomerpanel.py
@@ -5,92 +5,10 @@
 
 # Imports.--------------------------------------------------------------------
 # -Python Imports.
-import math
 
 # -wxPython Imports.
 import wx
-
-
-class ZMatrix(wx.AffineMatrix2D):
-    def __init__(self, *args, **kwds):
-        wx.AffineMatrix2D.__init__(self)
-
-    def Reset(self):
-        wx.AffineMatrix2D.__init__(self)
-
-    def PostScale(self, sx, sy=None, ax=0, ay=0):
-        self.Invert()
-        if sy is None:
-            sy = sx
-        if ax == 0 and ay == 0:
-            self.Scale(1.0 / sx, 1.0 / sy)
-        else:
-            self.Translate(ax, ay)
-            self.Scale(1.0 / sx, 1.0 / sy)
-            self.Translate(-ax, -ay)
-        self.Invert()
-
-    def PostTranslate(self, px, py):
-        self.Invert()
-        self.Translate(-px, -py)
-        self.Invert()
-
-    def PostRotate(self, angle, rx=0, ry=0):
-        self.Invert()
-        tau = math.pi * 2
-        if rx == 0 and ry == 0:
-            self.Rotate(-angle * tau / 360.0)
-        else:
-            self.Translate(rx, ry)
-            self.Rotate(-angle * tau / 360.0)
-            self.Translate(-rx, -ry)
-        self.Invert()
-
-    def PreScale(self, sx, sy=None, ax=0, ay=0):
-        if sy is None:
-            sy = sx
-        if ax == 0 and ay == 0:
-            self.Scale(sx, sy)
-        else:
-            self.Translate(ax, ay)
-            self.Scale(sx, sy)
-            self.Translate(-ax, -ay)
-
-    def PreTranslate(self, px, py):
-        self.Translate(px, py)
-
-    def PreRotate(self, angle, rx=0, ry=0):
-        tau = math.pi * 2
-        if rx == 0 and ry == 0:
-            self.Rotate(angle * tau / 360.0)
-        else:
-            self.Translate(rx, ry)
-            self.Rotate(angle * tau / 360.0)
-            self.Translate(-rx, -ry)
-
-    def GetScaleX(self):
-        return self.Get()[0].m_11
-
-    def GetScaleY(self):
-        return self.Get()[0].m_22
-
-    def GetSkewX(self):
-        return self.Get()[0].m_12
-
-    def GetSkewY(self):
-        return self.Get()[0].m_21
-
-    def GetTranslateX(self):
-        return self.Get()[1].x
-
-    def GetTranslateY(self):
-        return self.Get()[1].y
-
-    def InverseTransformPoint(self, position):
-        self.Invert()
-        converted_point = self.TransformPoint(position)
-        self.Invert()
-        return converted_point
+from zmatrix import ZMatrix
 
 
 class ZoomerPanel(wx.Panel):
